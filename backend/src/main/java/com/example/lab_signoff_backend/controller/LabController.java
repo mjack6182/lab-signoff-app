c124616d05895d9d048f70614ca05d801d7cc592
Jack Miller <148901348+mjack6182@users.noreply.github.com>
2025-09-18 22:57:31 -0500

    connected mongo atlas db and got springboot server running.

--- backend/src/main/java/com/example/lab_signoff_backend/controller/LabController.java
+++ backend/src/main/java/com/example/lab_signoff_backend/controller/LabController.java
@@ -1,4 +1,4 @@
-package main.java.com.example.lab_signoff_backend.controller;
+package com.example.lab_signoff_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
