790def9d884de3c6c6ecce23bfac9230dc3b85bb
Jack Miller <148901348+mjack6182@users.noreply.github.com>
2025-09-18 19:42:34 -0500

    added the Springboot backend using gradle groovy build. Still need mongo atlas db to connect

---
+++ backend/src/main/java/com/example/lab_signoff_backend/LabSignoffBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_signoff_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSignoffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSignoffBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/example/lab_signoff_backend/controller/LabController.java
@@ -0,0 +1,17 @@
+package main.java.com.example.lab_signoff_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class LabController {
+
+    @GetMapping("/labs")
+    public List<Map<String, Object>> getLabs() {
+        return List.of(
+                Map.of("id", "lab1", "title", "Lab 1 - Basics", "points", 3),
+                Map.of("id", "lab2", "title", "Lab 2 - Circuits", "points", 5));
+    }
+}
