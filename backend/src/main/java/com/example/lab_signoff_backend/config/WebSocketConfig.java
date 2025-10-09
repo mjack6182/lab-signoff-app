ebe6eacfe4f9a9947ec609ccc37c5f1ede639adf
ArjunNyein <152450502+ArjunNyein@users.noreply.github.com>
2025-10-09 18:35:12 -0500

    websocket changes
    Configured WebSocket to enable real-time communication between frontend and backend.
    
    Supported environment setup and ensured smooth integration for development and deployment.

--- backend/src/main/java/com/example/lab_signoff_backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/lab_signoff_backend/config/WebSocketConfig.java
@@ -20,17 +20,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(myWebSocketHandler, "/ws")
                 // Restrict allowed origins to trusted domains only
                 .setAllowedOrigins("http://localhost:3000", "https://yourdomain.com");
 =======
+=======
+>>>>>>> Stashed changes
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // Prefix for messages from server → clients
         registry.enableSimpleBroker("/topic");
 
         // Prefix for client → server destinations
         registry.setApplicationDestinationPrefixes("/app");
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
     }
 }
